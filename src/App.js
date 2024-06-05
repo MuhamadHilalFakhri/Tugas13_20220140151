8556daee584e461cd88b2da37ec20ac2dd71c21b
hilal555 <muhamadhilal04@gmail.com>
2024-06-05 14:46:29 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import sharingan from "./sharingan.png"; // Make sure the image file is in the 'src' directory
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-logo-left">
+          <img src={sharingan} className="App-logo" alt="logo" />
+        </div>
+        {/* Main content area */}
+        <div className="App-content">
+          <h1>Welcome to My React App</h1>
+          <p>Doa Programmer</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum..
+          </p>
+        </div>
+        <div className="App-logo-right">
+          <img src={sharingan} className="App-logo" alt="logo" />
+        </div>
       </header>
     </div>
   );
